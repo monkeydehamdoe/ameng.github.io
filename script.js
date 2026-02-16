a13781fa26df9f272a8f241656894e52666f4f32
monkeydehamdoe <hafizhfathurrohman62@gmail.com>
2026-02-16 23:41:29 +0700

    first commit

---
+++ script.js
@@ -0,0 +1,18 @@
+function login() {
+  var user = document.getElementById("username").value;
+  var pass = document.getElementById("password").value;
+
+  if (user === "ameng" && pass === "opengsukaameng") {
+    document.getElementById("pesan").innerHTML =
+      "Login berhasil! Mengalihkan...";
+    document.getElementById("pesan").style.color = "green";
+
+    // redirect setelah 1 detik
+    setTimeout(function () {
+      window.location.href = "story/index.html";
+    }, 1000);
+  } else {
+    document.getElementById("pesan").innerHTML = "Nak login telpon Openg dulu!";
+    document.getElementById("pesan").style.color = "red";
+  }
+}
